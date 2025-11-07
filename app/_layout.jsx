b029ce0e8ceea1b0fc189a88d0f578bf34a57852
Graziani Zanfolin <zanfolin@gmail.com>
2025-11-07 10:16:32 -0300

    [fix]  Tabbar color

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,32 +1,61 @@
 import { Tabs } from "expo-router";
-import FontAwesome from '@expo/vector-icons/FontAwesome';
+import FontAwesome from "@expo/vector-icons/FontAwesome";
 
 export default function Layout() {
   return (
-    <Tabs>
+    <Tabs
+      screenOptions={{
+        headerShown: false,
+        tabBarStyle: {
+          backgroundColor: "#1e1e2f", // cor de fundo do tab
+          borderTopColor: "transparent", // remove a borda superior
+          elevation: 10, // sombra no Android
+          height: 60, // altura da barra
+          paddingBottom: 5,
+        },
+        tabBarActiveTintColor: "#fff",
+        tabBarInactiveTintColor: "#888",
+      }}
+    >
       <Tabs.Screen
         name="index"
-        options={{ title: "Início", headerShown: false,
-            tabBarIcon: ({color}) => <FontAwesome name="home" size={24} color={color}/>
-         }}
+        options={{
+          title: "Início",
+          headerShown: false,
+          tabBarIcon: ({ color }) => (
+            <FontAwesome name="home" size={24} color={color} />
+          ),
+        }}
       />
       <Tabs.Screen
         name="pageone"
-        options={{ title: "Obrigatório", headerShown: false,
-            tabBarIcon: ({color}) => <FontAwesome name="tag" size={24} color={color}/>
-         }}
+        options={{
+          title: "Obrigatório",
+          headerShown: false,
+          tabBarIcon: ({ color }) => (
+            <FontAwesome name="tag" size={24} color={color} />
+          ),
+        }}
       />
       <Tabs.Screen
         name="pagetwo"
-        options={{ title: "Livre", headerShown: false,
-            tabBarIcon: ({color}) => <FontAwesome name="tags" size={24} color={color}/>
-         }}
+        options={{
+          title: "Livre",
+          headerShown: false,
+          tabBarIcon: ({ color }) => (
+            <FontAwesome name="tags" size={24} color={color} />
+          ),
+        }}
       />
-       <Tabs.Screen
+      <Tabs.Screen
         name="aboutme"
-        options={{ title: "Sobre", headerShown: false,
-            tabBarIcon: ({color}) => <FontAwesome name="user" size={24} color={color}/>
-         }}
+        options={{
+          title: "Sobre",
+          headerShown: false,
+          tabBarIcon: ({ color }) => (
+            <FontAwesome name="user" size={24} color={color} />
+          ),
+        }}
       />
     </Tabs>
   );
