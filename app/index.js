e073854d704850355c1bead78d4454b2a8a7cce8
Graziani Zanfolin <zanfolin@gmail.com>
2025-11-07 09:38:39 -0300

    First Commit

---
+++ app/index.js
@@ -0,0 +1,35 @@
+import { StyleSheet, Text, View } from "react-native";
+
+export default function Page() {
+  return (
+    <View style={styles.container}>
+      <View style={styles.main}>
+        <Text style={styles.title}>Início</Text>
+        <Text style={styles.subtitle}>Projeto de DDM</Text>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    padding: 24,
+    backgroundColor: "#e0e0e4ff",
+  },
+  main: {
+    flex: 1,
+    justifyContent: "center",
+    maxWidth: 960,
+    marginHorizontal: "auto",
+  },
+  title: {
+    fontSize: 64,
+    fontWeight: "bold",
+  },
+  subtitle: {
+    fontSize: 36,
+    color: "#38434D",
+  },
+});
